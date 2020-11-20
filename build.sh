04db10d2b7517c7228bddad293fbad743ffcb0bf
nganga <huyennga154@gmail.com>
2020-11-20 14:24:10 +0900

    Add build.sh file

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This file is used whenever resources are updated/changed
+# To build/generate war file using mvn, and then copy to destination folder
+cd /opt/manager-front
+make build
+wait
+echo "Front-end is already re-built!"
+cd /opt/manager-api
+mvn clean package
+echo "Api is already created!"
+wait
+sudo cp /opt/manager-api/target/eqsurv-manager.war /opt/TOMCAT_HOME/webapps/
