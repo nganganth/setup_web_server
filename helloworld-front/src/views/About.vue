c460b669dd38d9b1769b1060bf6939cef32a4926
nganga <huyennga154@gmail.com>
2020-11-25 12:15:21 +0900

    Add frontend's source code

---
+++ helloworld-front/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <div class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-size-1">About Animal Rescue League</h1>          
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <p class="org-description is-size-4">
+      More information.
+      </p>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+  .org-description {
+    margin-top: 50px;
+  }
+</style>
